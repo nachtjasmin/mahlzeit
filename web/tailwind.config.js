bc40e9367f3ba56b36e494a5973620f0bad29263
nachtjasmin <nachtjasmin@posteo.de>
2023-03-27 11:55:12 +0200

    Add additional font families

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,6 +4,25 @@ const config = {
 	content: ["./templates/**/*.tmpl"],
 	theme: {
 		extend: {
+			fontFamily: {
+				// taken from: https://modernfontstacks.com/
+				"old-style": [
+					"Iowan Old Style",
+					"Palatino Linotype",
+					"URW Palladio L",
+					"P052",
+					"serif",
+				],
+				"geometric-humanist": [
+					"Avenir",
+					"Avenir Next LT Pro",
+					"Montserrat",
+					"Corbel",
+					"URW Gothic",
+					"source-sans-pro",
+					"sans-serif",
+				],
+			},
 			colors: {
 				accent: colors.green,
 				neutral: colors.stone,
