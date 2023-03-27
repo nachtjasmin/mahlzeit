3f48532c893f4b7798c59faa1cdd23f2102b7cf4
nachtjasmin <nachtjasmin@posteo.de>
2023-03-27 11:55:33 +0200

    Add rainbow palette
    Reference: https://iamkate.com/data/12-bit-rainbow/

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -24,6 +24,21 @@ const config = {
 				],
 			},
 			colors: {
+				// Palette taken from Kate: https://iamkate.com/data/12-bit-rainbow/
+				rainbow: {
+					purple: "#817",
+					darkred: "#a35",
+					red: "#c66",
+					orange: "#e94",
+					yellow: "#ed0",
+					green: "#9d5",
+					lightgreen: "#4d8",
+					lightcyan: "#2cb",
+					cyan: "#0bc",
+					blue: "#09c",
+					darkblue: "#36b",
+					darkpurple: "#639",
+				},
 				accent: colors.green,
 				neutral: colors.stone,
 			},
